39a29ace7b51386644d7e11fc094b684a44687d9
komidawi <komidawi@gmail.com>
2019-11-08 10:38:12 +0100

    [PCC-2] Feat: Create basic UI for calculations

--- app/src/main/java/com/github/komidawi/pizzacostcalculator/calculations/Calculator.java
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/calculations/Calculator.java
@@ -3,12 +3,15 @@ package com.github.komidawi.pizzacostcalculator.calculations;
 import com.github.komidawi.pizzacostcalculator.pizza.PizzaShape;
 
 public class Calculator {
-    public static double calculateRatio(double diagonal, double price, PizzaShape pizzaShape) {
+    private static final int SQ_CENTIMETERS_IN_SQ_METRE = 10000;
+
+    // Calculates price / area ratio in currency units per one square meter of pizza
+    public static double calculateRatio(int diagonal, double price, PizzaShape pizzaShape) {
         double area = calculateArea(diagonal, pizzaShape);
-        return price / area;
+        return (price / area) * SQ_CENTIMETERS_IN_SQ_METRE;
     }
 
-    private static double calculateArea(double diagonal, PizzaShape pizzaShape) {
+    private static double calculateArea(int diagonal, PizzaShape pizzaShape) {
         switch (pizzaShape) {
             case CIRCLE:
                 return calculateCircleArea(diagonal);
@@ -21,11 +24,11 @@ public class Calculator {
         }
     }
 
-    private static double calculateCircleArea(double diagonal) {
+    private static double calculateCircleArea(int diagonal) {
         return Math.PI * Math.pow(diagonal / 2, 2);
     }
 
-    private static double calculateSquareArea(double diagonal) {
+    private static double calculateSquareArea(int diagonal) {
         return Math.pow(diagonal, 2) / 2;
     }
 }
